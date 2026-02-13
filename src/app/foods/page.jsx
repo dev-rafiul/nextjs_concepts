5a150fbec3ec471ba0d20ce98cc9e4abf151d108
COMRADE040 <rafiulislam040@gmail.com>
2026-02-14 00:50:27 +0600

    search bar implement

--- src/app/foods/page.jsx
+++ src/app/foods/page.jsx
@@ -1,18 +1,21 @@
 import FoodCard from '@/components/cards/FoodCard';
 import React from 'react';
+import CartItems from './CartItems';
+import InputSearch from '@/components/InputSearch';
 
 
 
-const getFoods = async() => {
-    const res = await fetch("https://taxi-kitchen-api.vercel.app/api/v1/foods/random")
+const getFoods = async(search) => {
+    const res = await fetch(` https://taxi-kitchen-api.vercel.app/api/v1/foods/random?search=${search}`)
     const data = await res.json()
     await new Promise((resolve) => setTimeout(resolve, 3000))
     return data.foods || [];
 }
 
-const FoodsPage = async() => {
+const FoodsPage = async({searchParams}) => {
+  const {search = ""} = await searchParams
  
-  const foods = await getFoods()
+  const foods = await getFoods(search)
   
 
 
@@ -24,13 +27,24 @@ const FoodsPage = async() => {
       <span className='text-yellow-500'> { foods.length} </span> Found
       </h2>
 
+      <div className='my-4'>
+        <InputSearch></InputSearch>
+      </div>
 
 
-      <div className='grid my-5 grid-cols-3 gap-5'>
+    <div className='flex'>
+      <div className='flex-1 grid my-5 grid-cols-3 gap-5'>
           {
             foods.map(food => <FoodCard key={food.id} food={food}></FoodCard>)
           }
+      </div> 
+
+      <div className='w-[250px] border-2 rounded p-4'>  
+        <h2 className='text-2xl font-bold'>Cart Items</h2> <hr />
+        <CartItems></CartItems>
+
       </div>
+          </div>
     </div>
   );
 };
--- src/components/buttons/CartButton.jsx
+++ src/components/buttons/CartButton.jsx
@@ -1,22 +1,27 @@
-"use client";
-import { CartContext } from "@/context/CartProvider";
-import React, { use, useState } from "react";
+"use client"
+import { CartContext } from '@/context/CartProvider';
+import React, { use, useState } from 'react';
 
-const CartButton = ({ food }) => {
-  const [inCart, setIncart] = useState(false);
-  const { addToCart } = use(CartContext);
+const CartButton = ({food}) => {
+
+  
+  const [inCart, setInCart] = useState(false)
+  const {addToCart} = use(CartContext);
   const handleadd2Cart = () => {
     addToCart(food);
-    setIncart(true);
-  };
+    setInCart(true)
+  }
+
+
+
+  // const handleadd2Cart = () => {
+  //   setInCart(true)
+  // }
 
   return (
-    <button
-      onClick={handleadd2Cart}
-      disabled={inCart}
-      className="flex-1 bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 disabled:bg-gray-100 disabled:text-gray-400"
-    >
-      {inCart ? "Added" : "Add to Cart"}
+    <button onClick={handleadd2Cart} disabled={inCart} className='flex-1 bg-blue-600 text-white py-2 rounded-lg disabled:bg-amber-50 disabled:text-gray-500'>
+      {inCart ? "Added" : "Add To Cart"}
+      
     </button>
   );
 };
