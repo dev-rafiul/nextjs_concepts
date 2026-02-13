1f715d7497c86515ffde417f5818b9dd23fff518
COMRADE040 <rafiulislam040@gmail.com>
2026-02-13 19:43:43 +0600

    first commit

--- src/app/foods/loading.jsx
+++ src/app/foods/loading.jsx
@@ -4,9 +4,11 @@ import React from "react";
 const loading = () => {
   return (
     <div className="grid my-5 grid-cols-3 gap-5">
-      {[...Array(12)].map((_, index) => (
+      {
+      [...Array(12)].map((_, index) => (
         <FoodCardSkeleton key={index}></FoodCardSkeleton>
-      ))}
+      ))
+      }
     </div>
   );
 };
